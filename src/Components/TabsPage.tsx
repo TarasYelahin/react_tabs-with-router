27be743e4c6575d436483616d2386b707fe5d434
Taras Yelahin <inflades@gmail.com>
2026-04-27 15:24:05 +0300

    fix_01

--- src/Components/TabsPage.tsx
+++ src/Components/TabsPage.tsx
@@ -1,35 +1,34 @@
-import { useParams, Link } from 'react-router-dom';
-
+import { Tabs, TabList, Tab, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import { useParams, useNavigate } from 'react-router-dom';
 export const TabsPage = () => {
   const tabs = [
     { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
     { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
     { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
   ];
-
   const { tabId } = useParams();
-  const activeTab = tabs.find(tab => tab.id === tabId);
+  const navigate = useNavigate();
+  const activeTabIndex = tabs.findIndex(tab => tab.id === tabId);
 
   return (
     <>
       <h1 className="title">Tabs page</h1>
-      <div className="tabs is-boxed">
-        <ul>
+      <Tabs
+        selectedIndex={activeTabIndex >= 0 ? activeTabIndex : 0}
+        onSelect={index => navigate(`/tabs/${tabs[index].id}`)}
+      >
+        <TabList>
           {tabs.map(tab => (
-            <li
-              key={tab.id}
-              data-cy="Tab"
-              className={tab.id === tabId ? 'is-active' : ''}
-            >
-              <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
-            </li>
+            <Tab key={tab.id} data-cy="Tab">
+              {tab.title}
+            </Tab>
           ))}
-        </ul>
-      </div>
-
-      <div className="block" data-cy="TabContent">
-        {activeTab ? activeTab.content : 'Please select a tab'}
-      </div>
+        </TabList>
+        {tabs.map(tab => (
+          <TabPanel key={tab.id}>{tab.content}</TabPanel>
+        ))}
+      </Tabs>
     </>
   );
 };
